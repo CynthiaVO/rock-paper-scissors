b97ff7459f10241befc3aaae4ee461afd367e44a
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-09 08:21:17 -0800

    function returns a choice for computer

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
 function getComputerChoice() {
   //randomly assigns the computer choice from either rock, paper or scissors
   //randomly generate 1 of 3 numbers
-  computerChoice = Math.floor(Math.random() * (max - min)) + min;
-  console.log(computerChoice);
   //assign the choice which corresponds to that number to the computer
+  var computerChoice = Math.floor(Math.random() * (4 - 1)) + 1;
+  //assign the computer choice of rock paper scissors based on number
   //return the choice
+  if (computerChoice == 1)
+    return "rock";
+  if (computerChoice == 2)
+    return "paper";
+  else
+    return "scissors";
+  
+  
+  return computerChoice;
 }
+
+console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
   // your code here!
