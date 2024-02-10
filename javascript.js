0dd3a6f089606893d174658f5b42b2de6870e770
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-10 10:08:31 -0800

    prints user and computer choice

--- javascript.js
+++ javascript.js
@@ -1,23 +1,36 @@
+function getUserChoice() {
+  // ask for player to input selection
+  let playerChoice = "rock";
+  return playerChoice;
+}
 function getComputerChoice() {
   //randomly assigns the computer choice from either rock, paper or scissors
   //randomly generate 1 of 3 numbers
   //assign the choice which corresponds to that number to the computer
-  var computerChoice = Math.floor(Math.random() * (4 - 1)) + 1;
+  var randChoice = Math.floor(Math.random() * (4 - 1)) + 1;
   //assign the computer choice of rock paper scissors based on number
   //return the choice
-  if (computerChoice == 1)
-    return "rock";
-  if (computerChoice == 2)
-    return "paper";
-  else
-    return "scissors";
-  
-  
+  let computerChoice;
+  if (randChoice == 1) {
+    computerChoice = "rock";
+    //document.getElementById("computerChoice").innerHTML = "rock";
+  }  
+  else if (randChoice == 2) {
+    computerChoice = "paper";
+    //document.getElementById("computerChoice").innerHTML = "paper";
+  }
+  else {
+    computerChoice = "scissors";
+    //document.getElementById("computerChoice").innerHTML = "scissors";
+  }
+  console.log(computerChoice);
   return computerChoice;
 }
+let computerChoice = getComputerChoice();
 
-console.log(getComputerChoice());
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
+//playRound(playerChoice, computerChoice);
+//function playRound(playerSelection, computerSelection) {
+  
+  //compare their selection to the computer selection
+  //output winner
+//}
